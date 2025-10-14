018e17797dc628ecd2bbf7be863087b197388275
Manikanta <omanikanta@teksystems.com>
2025-10-14 20:41:44 +0530

    basic skeleton code

---
+++ src/test/java/com/gha/GhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
