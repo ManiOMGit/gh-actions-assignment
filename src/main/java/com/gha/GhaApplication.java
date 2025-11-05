55cf23e3da901cf1ff5297eeb43638a9a79418b4
Manikanta <omanikanta@teksystems.com>
2025-11-05 10:47:47 +0530

    HotFix to main

--- src/main/java/com/gha/GhaApplication.java
+++ src/main/java/com/gha/GhaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GhaApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hey I am hotfix in main branch");
 		SpringApplication.run(GhaApplication.class, args);
 	}
 
