4ad120c4eb01bb951759393c91bb900154e71761
Manikanta <omanikanta@teksystems.com>
2025-11-05 11:06:37 +0530

    update main method in develop branch

--- src/main/java/com/gha/GhaApplication.java
+++ src/main/java/com/gha/GhaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class GhaApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hey I am hotfix in main branch");
+		System.out.println("Hey I am hotfix in main branch and added another feature in develop");
 		SpringApplication.run(GhaApplication.class, args);
 	}
 
